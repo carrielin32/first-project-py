b997168be4d936529e4b0e02af4308b89365bdc5
Carrielin <30627002+carrielin32@users.noreply.github.com>
2017-08-22 17:16:08 +0800

    Add files via upload

--- acc.py
+++ acc.py
@@ -5,13 +5,17 @@ import numpy as np
 # import matplotlib.pyplot as plt 
 
 df8=pd.read_csv('/Users/carrielin/Desktop/result/8_result_2.csv')
+
+df8['stimuli']=df8['stimuli'].str[:3]
+
 df8['stimuli']=df8['stimuli'].replace('ba2','ba,tone2')
 df8['stimuli']=df8['stimuli'].replace('ba4','ba,tone4')
 df8['stimuli']=df8['stimuli'].replace('da2','da,tone2')
 df8['stimuli']=df8['stimuli'].replace('da4','da,tone4')
 
 df8_acc=df8['stimuli']
-df8_acc[0].str.split(',',1,expand=True).to_csv('Users/carrielin/Desktop/result/8_result_acc.csv')
+df8_acc=df8_acc.get_values()
+df8_acc[0].str.split(',',1,expand=True).to_csv('/Users/carrielin/Desktop/result/8_result_acc.csv')
 
 
 df8_acc=pd.read_csv('/Users/carrielin/Desktop/result/8_result_acc.csv')
